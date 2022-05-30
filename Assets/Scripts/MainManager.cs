761b032ea0cecdbaa181508509fe17768afedc15
DzhangiTau <pavelgelazarishvili@gmail.com>
2022-05-30 19:30:19 +0400

    Add object for passing data between scenes

---
+++ Assets/Scripts/MainManager.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainManager : MonoBehaviour
+{
+    public static MainManager Instance;
+    public Color teamColor;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+}
