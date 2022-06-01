90eebeca82f6550783724341bb09d82f81dda6f9
DzhangiTau <pavelgelazarishvili@gmail.com>
2022-06-01 08:24:08 +0400

    Make productivity unit increase production speed

--- Assets/Scripts/ProductivityUnit.cs
+++ Assets/Scripts/ProductivityUnit.cs
@@ -4,9 +4,37 @@ using UnityEngine;
 
 public class ProductivityUnit : Unit
 {
+    private ResourcePile m_CurrentPile = null;
+    public float ProductivityMultiplier = 2;
+
+
     protected override void BuildingInRange()
     {
-        throw new System.NotImplementedException();
+        if (m_CurrentPile == null)
+        {
+            ResourcePile pile = m_Target as ResourcePile;
+
+            if (pile != null)
+            {
+                m_CurrentPile = pile;
+                m_CurrentPile.ProductionSpeed *= ProductivityMultiplier;
+            }
+        }
+    }
+
+    public override void GoTo(Vector3 position)
+    {
+        ResetProductivity();
+        base.GoTo(position);
+    }
+
+    void ResetProductivity()
+    {
+        if (m_CurrentPile != null)
+        {
+            m_CurrentPile.ProductionSpeed /= ProductivityMultiplier;
+            m_CurrentPile = null;
+        }
     }
 
     // Start is called before the first frame update
@@ -18,6 +46,6 @@ public class ProductivityUnit : Unit
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
